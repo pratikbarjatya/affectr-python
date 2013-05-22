50af75142b21fcacfe496cb19e59f9daa23a5df2
Mathias Bogaert <mathias.bogaert@gmail.com>
2013-05-22 16:45:02 +0200

    Initial entry.

---
+++ affectr/__init__.py
@@ -0,0 +1,42 @@
+
+"""TheySay Affectr Python Client Library
+
+This module provides a wrapper around the TheySay Affectr API, the
+interface to the api is provided by the :py:class:`affectr.Client`
+object. See the documentation for that class for how to obtain a reference.
+
+"""
+
+VERSION = (0, 1, 0)
+
+def get_version():
+    return '.'.join(str(part) for part in VERSION)
+
+from .client import Client
+
+#import as clientlib so that we don't shadow with the client variable
+import client as clientlib
+from affectr.resources import (Bill, Subscription, PreAuthorization, User,
+                                  Merchant)
+
+client = None
+"""The client to use. Does not exist until
+:py:func:`affectr.set_details` has been called.
+"""
+
+
+def set_details(username=None, password=None):
+    """Set the global account details to use for requests
+
+    The parameters are your security details which can be found
+    on your TheySay developer details page. All are mandatory.
+    """
+    if ysername is None:
+        raise ValueError("username is required")
+    if password is None:
+        raise ValueError("password is required")
+
+    global client
+    client = Client(app_id, app_secret, access_token=access_token,
+                    merchant_id=merchant_id)
+
---
+++ affectr/request.py
@@ -0,0 +1,36 @@
+import affectr
+import json
+import requests
+
+class Request(object):
+
+    def __init__(self, method, url):
+        self._method = method
+        self._url = url
+        headers = {}
+        headers["Accept"] = "application/json"
+        lib_version = affectr.get_version()
+        headers["User-Agent"] = "affectr-python/{0}".format(lib_version)
+        self._opts = {"headers": headers}
+
+        if not self._valid_method(method):
+            raise ValueError('Invalid method {0}'.format(method))
+
+    def _valid_method(self, method):
+        return method in ('get', 'post', 'put', 'delete')
+
+    def use_http_auth(self, username, password):
+        self._opts['auth'] = (username, password)
+
+    def set_payload(self, payload):
+        if payload is not None:
+            # Set the payload type - always JSON
+            self._opts['headers']['Content-Type'] = 'application/json'
+            # And JSON encode the data
+            self._opts['data'] = json.dumps(payload)
+
+    def perform(self):
+        fetch_func = getattr(requests, self._method)
+        response = fetch_func(self._url, **self._opts)
+        return json.loads(response.content)
+
